e3499b9ec521fc8179c714fed445e81d490b41ea
Joshua Baxter <baxter.josh@gmail.com>
2015-12-05 00:47:06 -0800

    working on a menu screen

---
+++ win2d_text_game_world_generator/game/MainMenu.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Graphics.Canvas.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI;
+
+namespace win2d_text_game_world_generator
+{
+    public static class MainMenu
+    {
+        private static Rect Rect;
+        public static List<MenuItem> MenuItems = new List<MenuItem>();
+
+        private static int _selectedindex;
+        private static MenuItem _selecteditem;
+        public static MenuItem SelectedItem { get { return _selecteditem; } }
+
+        public static void Draw(CanvasAnimatedDrawEventArgs args)
+        {
+            DrawBackground(args);
+            DrawItems(args);
+        }
+
+        private static void DrawBackground(CanvasAnimatedDrawEventArgs args)
+        {
+            args.DrawingSession.FillRectangle(Rect, Colors.CornflowerBlue);
+        }
+
+        private static void DrawItems(CanvasAnimatedDrawEventArgs args)
+        {
+            for (int i = 0; i < MenuItems.Count; i++)
+            {
+                if (i == _selectedindex)
+                {
+                    MenuItems[i].DrawSelected(args);
+                }
+                else
+                {
+                    MenuItems[i].Draw(args);
+                }
+            }
+        }
+
+        public static void ScrollDown()
+        {
+            if (MenuItems.Count == 0) { return; }
+
+            _selectedindex = (_selectedindex + 1) % MenuItems.Count;
+            _selecteditem = MenuItems[_selectedindex];
+        }
+
+        public static void ScrollUp()
+        {
+            if (MenuItems.Count == 0) { return; }
+
+            _selectedindex = (_selectedindex - 1) % MenuItems.Count;
+            _selecteditem = MenuItems[_selectedindex];
+        }
+    }
+}
---
+++ win2d_text_game_world_generator/game/MenuItem.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Text;
+using Microsoft.Graphics.Canvas.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+
+namespace win2d_text_game_world_generator
+{
+    public class MenuItem
+    {
+        private CanvasTextLayout _textlayout;
+        public CanvasTextLayout TextLayout { get { return _textlayout; } }
+
+        private Vector2 _position;
+        public Vector2 Position { get { return _position; } }
+
+        public MenuItem(CanvasDevice device, string text, Vector2 position)
+        {
+            _textlayout = new CanvasTextLayout(device, text, Statics.FontMedium, 0, 0);
+            _position = position;
+        }
+
+        public void Draw(CanvasAnimatedDrawEventArgs args)
+        {
+            args.DrawingSession.DrawTextLayout(TextLayout, Position, Colors.White);
+        }
+
+        public void DrawSelected(CanvasAnimatedDrawEventArgs args)
+        {
+            args.DrawingSession.DrawTextLayout(TextLayout, Position, Colors.Yellow);
+        }
+    }
+}
